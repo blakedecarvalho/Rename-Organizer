5982be444db2bede144f0b34852707d70fa83b9b
malbolge <malbolge@tuta.io>
2017-04-04 21:16:36 -0400

    fixed bug

--- Renamer.rb
+++ Renamer.rb
@@ -3,13 +3,13 @@ irb
 require 'pathname'
 require 'fileutils'
 names = Dir.entries(".")
-i = 4
+i = 2
 begin
-  while i < 98
+  while i < 3
     folder = "/Volumes/The Potion/JW/Poetry/#{names[i]}"
     Dir.glob("#{folder}/**/*.mov").each do |f|
       john = folder.gsub(/.+Poetry./, "")
-      name = File.absolute_path(f)
+      name = File.dirname(f)
       File.rename(f, name + " by #{john}" + File.extname(f))
       print names[i]
     end
