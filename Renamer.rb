fd7aad960c201345342532180e45e57eb2d4aa96
malbolge <malbolge@tuta.io>
2017-04-05 20:55:39 -0400

    final edit: added rolling directory array

--- Renamer.rb
+++ Renamer.rb
@@ -5,13 +5,14 @@ require 'fileutils'
 names = Dir.entries(".")
 i = 2
 begin
-  while i < 3
+  while i < 110
     folder = "/Volumes/The Potion/JW/Poetry/#{names[i]}"
     Dir.glob("#{folder}/**/*.mov").each do |f|
       john = folder.gsub(/.+Poetry./, "")
       name = File.dirname(f)
-      File.rename(f, name + " by #{john}" + File.extname(f))
-      print names[i]
+      newspot = File.dirname(f) + " by #{john}" + File.extname(f)
+      File.rename(f, newspot)
+      FileUtils.mv(newspot, name)
     end
     i+=1
   end
